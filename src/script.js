941161e89edd623201f22e736f9b00887c8d4010
Yuliia <yuliapysarenko@gmail.com>
2024-04-12 00:18:55 +0300

    Update draco file path

--- src/script.js
+++ src/script.js
@@ -23,7 +23,7 @@ const scene = new THREE.Scene();
 
 // Loaders
 const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath("/draco/");
+dracoLoader.setDecoderPath("./draco/");
 
 const gltfLoader = new GLTFLoader();
 gltfLoader.setDRACOLoader(dracoLoader);
